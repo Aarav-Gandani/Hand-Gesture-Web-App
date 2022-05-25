39547c6ae1ac7fbb7e669273eba552db6ba7a34b
Aarav-Gandani <82732060+Aarav-Gandani@users.noreply.github.com>
2022-05-25 12:31:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,11 +13,38 @@ png_quality:90
      });
  }
  console.log('ml5 version', ml5.version);
- classifier=ml5.imageClassifer('https://teachablemachine.withgoogle.com/models/5mxzZHkpx/model.json', modelLoaded);
+ classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/z1FzPpz5K/model.json', modelLoaded);
 
  function modelLoaded(){
      console.log('Model Loaded');
  }
+ function check(){
+    img=document.getElementById('captured_image');
+classifier.classify(img, gotResult);
+    }
+    
+    function gotResult(error, results){
+      if (error){
+          console.error(error);
+      } else{
+          console.log(results);
+          document.getElementById("result_gesture_name").innerHTML=results[0].label;
+          prediction=results[0].label;
+          speak();
+          if(results[0].label=="Yo Bruh"){
+              document.getElementById("update_gesture").innerHTML="	&#129304;"
+          }
+          if(results[0].label=="Ok Dude"){
+            document.getElementById("update_gesture").innerHTML="   &#128076;"
+        }
+        if(results[0].label=="Hi Bro"){
+            document.getElementById("update_gesture").innerHTML="&#128400;"
+        }
+        if(results[0].label=="One Min"){
+            document.getElementById("update_gesture").innerHTML="&#9757;"
+        }
+      }
+    }
  function speak(){ 
 var synth = window.speechSynthesis;
 speak_data = "The prediction is " + prediction;  
